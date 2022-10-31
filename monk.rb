a7e572a4435fe290bae572d62cc7566a8c05c421
alexandros514 <66693474+alexandros514@users.noreply.github.com>
2022-10-31 14:27:55 +0200

    Update monk.rb

--- monk.rb
+++ monk.rb
@@ -3,11 +3,15 @@ def keke(x)
 end
 
 def menu
+  puts "welcome to kekeifier"
   loop do
   puts "whats the word i need to see?"
   u_input= gets.chomp().to_s
-   if u_input == "qq"
-     break
+    if u_input == "qq"
+      puts "OK bye bye :()"
+      break
+    else
+       puts keke(u_input)
    end
  end
 end
