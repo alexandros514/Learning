467a1641207cf4200cea546ed09e5f6a35297a9d
alexandros514 <66693474+alexandros514@users.noreply.github.com>
2022-10-31 20:50:24 +0200

    finish

--- monk.rb
+++ monk.rb
@@ -4,30 +4,29 @@ end
 
 
 def long_string(a)
-  t=a.length.to_i
-  if t >= 10
-    return true
-  else
-    return false
-  end
+  a.length.to_i >= 10
 end
-
-
 
 def menu
   puts "==Welcome to Kekeifier=="
   loop do
-  puts "whats the word i need to see?"
-  u_input= gets.chomp().to_s
+    puts "whats the word i need to see?"
+    u_input= gets.chomp().to_s
     if u_input == "qq"
       puts "OK bye bye :("
       break
     elsif long_string(u_input)
-      puts "phrase too long to KEKE... ." + "(#{u_input})"
+      puts "phrase too long to KEKE... .(#{u_input})"
     else
-       puts "Your KEKE phrase: " +keke(u_input)
-   end
- end
+      newkeke=keke(u_input)
+      puts "Your KEKE phrase: " + newkeke
+      puts "save it?[Y][N]"
+      answer=gets.chomp()
+      if answer == "Y"
+        File.write("monk.txt", "Your KEKE phrase: #{newkeke}")
+      end
+    end
+  end
 end
 
 menu
